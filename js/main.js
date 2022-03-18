23d05ea210e5335f84475c73e0dea4bbfb0a50c7
sendeiser <87587210+sendeiser@users.noreply.github.com>
2022-03-17 23:32:15 -0300

    Second Commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+( ()=> {
+    const imagenes = [...document.querySelectorAll('.elem-asside')];
+    const galery = document.querySelector('.contenedor_img img');
+  
+    imagenes.map((elem)=>{
+      console.log(getComputedStyle(elem).backgroundImage);
+      
+    })
+    const ultima= getComputedStyle(imagenes[0]).backgroundImage;
+    galery.src=ultima.substr(26,15);
+    
+    console.log(ultima.substr(26,15));
+}
+)()
