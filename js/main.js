18f2c797e39aa24b9b9a9dd341c1ccf4ad3103ca
sendeiser <87587210+sendeiser@users.noreply.github.com>
2022-03-18 12:28:51 -0300

    Arrglos Finales v1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 ( ()=> {
     const imagenes = [...document.querySelectorAll('.elem-asside')];
-    const galery = document.querySelector('.contenedor_img img');
+    const galery = document.querySelector('.contenedor_img .img');
   
-    imagenes.map((elem)=>{
-      console.log(getComputedStyle(elem).backgroundImage);
-      
-    })
-    const ultima= getComputedStyle(imagenes[0]).backgroundImage;
+    function cambio() {
+      galery.style.backgroundImage=getComputedStyle(this).backgroundImage;
+    }
+    imagenes.forEach(img => img.addEventListener('click', cambio));
+
+   /*  const ultima= getComputedStyle(imagenes[0]).backgroundImage;
     galery.src="."+ultima.substr(49,15);
     
-    console.log(ultima.substr(50,15));
+    console.log(ultima.substr(50,15)); */
 }
 )()
